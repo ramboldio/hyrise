443f8066bc80663a731b97b8e2d27d47a6757646
Lukas Rambold <lukas@rambold.io>
2020-04-12 17:25:33 +0200

    SytheticTableGenerator can't generate NULL columns
    For the column calibration we wanted to explore how NULL values affect Scan performance.
    During this we encountered that with the current version of
    the Synthetic Table Generator it is not possible to generate these values.
    
    Is this a bug?
    
    ```
    terminate called after throwing an instance of 'std::logic_error'
      what():  src/lib/resolve_type.hpp:76 data_type cannot be null.
    
    Process finished with exit code 134 (interrupted by signal 6: SIGABRT)
    ```

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <synthetic_table_generator.hpp>
 
 #include "types.hpp"
 
 using namespace opossum;  // NOLINT
 
 int main() {
-  std::cout << "Hello world!!" << std::endl;
+  auto column_specs = std::vector<ColumnSpecification>();
+  column_specs.emplace_back(ColumnSpecification(ColumnDataDistribution::make_uniform_config(0.0, 1000.0), DataType::Null, EncodingType::Unencoded, "yo"));
+  const auto table = SyntheticTableGenerator().generate_table(column_specs, 10, 10, UseMvcc::Yes);
+
   return 0;
 }
